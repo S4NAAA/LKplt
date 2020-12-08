d8013c6e7e06af4a5af9ab943ea5064ecd665d7f
s4naaa <s8430801@gmail.com>
2020-12-08 14:59:58 -0600

    formating issues

--- NwtPly.py
+++ NwtPly.py
@@ -1,55 +1,33 @@
 from Poly import Poly
 
 class NwtPly(Poly):
-	def __init__(self, x, fx):
+  def __init__(self, x, fx):
 
-		if len(x) != len(fx):
-			raise Exception("Invalid x - fx pair values")
+    if len(x) != len(fx):
+      raise Exception("Invalid x - fx pair values")
 
-		tmp1 = fx[:]
-		tmp2 = [0] * len(x)
-		coeff = [0] * len(x)
-
-
-		for i in range(len(tmp1) - 1):
-			for j in range(len(tmp1) - i - 1):
-				tmp2[j] = (tmp1[j + 1] - tmp1[j]) / (x[j + i + 1] - x[j])
-				coeff[i + 1] = tmp2[0]
-			tmp1 = tmp2
-		
-		tmpPly = Poly([fx[0]])
-
-		for i in range(len(x)):
-			toAdd = Poly ([coeff[i]])
-			for j in range(i):
-				toAdd = toAdd * Poly([-x[j], 1])
-
-			tmpPly = tmpPly + toAdd
-
-		super().__init__(tmpPly.coeff)
+    tmp1 = fx[:]
+    tmp2 = [0] * len(x)
+    coeff = [0] * len(x)
 
 
-class NwtErrPly(Poly):
-	def __init__(self, x, fx):
+    for i in range(len(tmp1) - 1):
+      for j in range(len(tmp1) - i - 1):
+        tmp2[j] = (tmp1[j + 1] - tmp1[j]) / (x[j + i + 1] - x[j])
+        coeff[i + 1] = tmp2[0]
+      tmp1 = tmp2
+  
+    tmpPly = Poly([fx[0]])
 
-		if len(x) != len(fx):
-			raise Exception("Invalid x - fx pair values")
+    for i in range(len(x)):
+      toAdd = Poly ([coeff[i]])
+      for j in range(i):
+        toAdd = toAdd * Poly([-x[j], 1])
 
-		tmp1 = fx[:]
-		tmp2 = [0] * len(x)
-		coeff = [0] * len(x)
+      tmpPly = tmpPly + toAdd
+
+    super().__init__(tmpPly.coeff)
 
 
-		for i in range(len(tmp1) - 1):
-			for j in range(len(tmp1) - i - 1):
-				tmp2[j] = (tmp1[j + 1] - tmp1[j]) / (x[j + i + 1] - x[j])
-				coeff[i + 1] = tmp2[0]
-			tmp1 = tmp2
-		
-		fm = Poly([coeff[-1]])
 
-		for xv in x[:-1]:
-			fm *= Poly([-xv, 1])
-
-		super().__init__(fm.coeff) 
 	
